2b2a19a06b4a3c5576a5faeba9675c80fff0099d
NaJiwoong <chemistna1803@gmail.com>
2020-01-05 20:33:33 +0900

    login_only

--- myapplication/src/main/java/com/example/myapplication/Login.java
+++ myapplication/src/main/java/com/example/myapplication/Login.java
@@ -16,8 +16,12 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -29,7 +33,7 @@ public class Login extends Activity {
 
     LoginButton loginButton;
     private CallbackManager callbackManager;
-    String connectUrl = "http://192.168.0.100/3000";
+    String connectUrl = "http://192.168.0.100:3000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -40,66 +44,24 @@ public class Login extends Activity {
 
         Button button1=(Button) findViewById(R.id.login_bt);
         Button button2=(Button) findViewById(R.id.registration_bt);
-        EditText edid = (EditText) findViewById(R.id.idText);
-        EditText edps = (EditText) findViewById(R.id.passwordText);
-        final String id = edid.getText().toString();
-        final String pass = edps.getText().toString();
+//        EditText edid = (EditText) findViewById(R.id.idText);
+//        EditText edps = (EditText) findViewById(R.id.passwordText);
+//        final String id = edid.getText().toString();
+//        final String pass = edps.getText().toString();
+
 
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-
-                AsyncTask<String, Void, String> asyncTask = new AsyncTask<String, Void, String>() {
-                    @Override
-                    protected String doInBackground(String... strings) {
-                        try {
-                            URL url = new URL(connectUrl);
-                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                            conn.setDoOutput(true);
-                            conn.setRequestMethod("GET");
-                            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
-
-                            String params=String.format("/login/%s/%s", id, pass);
-                            osw.write(params);
-                            osw.flush();
-
-                            BufferedReader br = null;
-                            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-
-                            String line = null;
-                            line = br.readLine();
-                            if (line == "success"){
-                                return line;
-                            }else{
-                                Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
-                                return null;
-                            }
-
-                        }catch(MalformedURLException e){
-                            Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
-                        }catch(IOException e){
-                            Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
-                        }
-
-                        return null;
-                    }
-                };
-                String result = "failure";
-                try {
-                    result = asyncTask.execute(connectUrl, null, connectUrl).get();
-
-                }catch(Exception e){
-                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
-                }
-                if (result == "success"){
-                    Intent intent = new Intent(Login.this, MainActivity.class);
-
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View view){
+                EditText edid = (EditText) findViewById(R.id.idText);
+                EditText edps = (EditText) findViewById(R.id.passwordText);
+                String id = edid.getText().toString();
+                String pass = edps.getText().toString();
+                Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
+                new JSONTask().execute(connectUrl + "/login", id, pass);
             }
         });
+
 
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -113,7 +75,7 @@ public class Login extends Activity {
 
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
-        // If using in a fragmen
+        // If using in a fragment
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -143,4 +105,97 @@ public class Login extends Activity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    /* 여기부터 JSONTask 만들기 */
+
+    public class JSONTask extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... urls){
+            try{
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("id", urls[1]);
+                jsonObject.put("password", urls[2]);
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    URL url = new URL(urls[0]);
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Cache-Control", "no-cache");
+                    con.setRequestProperty("Content-Type", "application/json");
+
+                    con.setRequestProperty("Accept", "text/html");
+                    con.setDoOutput(true);
+                    con.setDoInput(true);
+
+                    con.connect();
+
+                    OutputStream outStream = con.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter((outStream)));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();
+
+                    // Create Input Stream
+                    InputStream stream = con.getInputStream();
+
+                    // Create buffer to improve speed and lessen burden
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    // Where get data in real
+                    StringBuffer buffer = new StringBuffer();
+
+                    // variance for getting lines' strings respectively
+                    String line = "";
+
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally{
+                    if (con != null){
+                        con.disconnect();
+                    }try{
+                        if (reader != null){
+                            reader.close();
+                        }
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }//finally part
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return "failure";
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            if (result.equals("success")){
+                Intent intent = new Intent(Login.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Failed to login", Toast.LENGTH_SHORT).show();
+            }
+            return;
+
+        }
+
+
+    }
+
 }
+
+
+
