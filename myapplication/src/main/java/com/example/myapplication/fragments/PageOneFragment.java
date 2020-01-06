0e93fd7c7a8bc5a039e52d95622d8abf88e3f8c1
오승준 <tmdwns0907@naver.com>
2020-01-06 19:54:53 +0900

    tab1

--- myapplication/src/main/java/com/example/myapplication/fragments/PageOneFragment.java
+++ myapplication/src/main/java/com/example/myapplication/fragments/PageOneFragment.java
@@ -2,8 +2,10 @@ package com.example.myapplication.fragments;
 
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
@@ -29,13 +31,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class PageOneFragment extends Fragment {
     ListView mListView;
-
+    String connectUrl = "http://192.168.0.100:3000";
 
     public PageOneFragment() {
     }
@@ -47,6 +58,11 @@ public class PageOneFragment extends Fragment {
         mListView = (ListView)rootview.findViewById(R.id.listView1);
         mListView.setAdapter(mMyAdapter);
 
+        ArrayList<String> sendParam=new ArrayList<>();
+        sendParam.add(connectUrl + "/address");
+        SharedPreferences pref = this.getActivity().getSharedPreferences("login", MODE_PRIVATE);
+        String sesId = pref.getString("id", "");
+        sendParam.add(sesId);
 
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
 
@@ -78,16 +94,95 @@ public class PageOneFragment extends Fragment {
 
 
                 mMyAdapter.addItem(ContextCompat.getDrawable(getContext(), R.drawable.vfdvdfdfv),contactCursor.getString(2), phonenumber);
+                sendParam.add(contactCursor.getString(2));
+                sendParam.add(phonenumber);
             } while (contactCursor.moveToNext());
         }
 
 
+        //String sesPass = pref.getString("password", "");
+        new SendContacts().execute(sendParam);
 
         return rootview;
 
     }
 
+    public class SendContacts extends AsyncTask<ArrayList<String>, String, String> {
 
+        @Override
+        protected String doInBackground(ArrayList<String>... urls) {
+            try {
+                JSONObject jsonObject = new JSONObject();
+                JSONObject jsonObject2 = new JSONObject();
+                jsonObject.put("id", urls[0].get(1));
+
+                for (int i = 2; i < urls[0].size(); i = i + 2) {
+                    jsonObject2.put(urls[0].get(i + 1), urls[0].get(i));
+                }
+                //jsonObject.put("contacts", urls[2]);
+
+                jsonObject.put("address", jsonObject2.toString());
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try {
+                    URL url = new URL(urls[0].get(0));
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Cache-Control", "no-cache");
+                    con.setRequestProperty("Content-Type", "application/json");
+
+                    con.setRequestProperty("Accept", "text/html");
+                    con.setDoOutput(true);
+                    con.setDoInput(true);
+
+                    con.connect();
+
+                    OutputStream outStream = con.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter((outStream)));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();
+
+                    // Create Input Stream
+                    InputStream stream = con.getInputStream();
+
+                    // Create buffer to improve speed and lessen burden
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    // Where get data in real
+                    StringBuffer buffer = new StringBuffer();
+
+                    // variance for getting lines' strings respectively
+                    String line = "";
+
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (con != null) {
+                        con.disconnect();
+                    }
+                    try {
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }//finally part
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "failure";
+        }
+    }
 
     private String getJsonString()
     {
