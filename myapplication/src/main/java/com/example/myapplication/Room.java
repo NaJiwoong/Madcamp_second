052a59b24b0b604dc4c289f112f89ae3626ba727
오승준 <tmdwns0907@naver.com>
2020-01-07 16:16:56 +0900

    get_room_list

---
+++ myapplication/src/main/java/com/example/myapplication/Room.java
@@ -0,0 +1,191 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Room extends Activity implements View.OnClickListener {
+
+    int mcompare=0;
+    int frequency = 8000;
+    int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+    int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+
+    private ca.uol.aig.fftpack.RealDoubleFFT transformer;
+    int blockSize = 256;
+
+    Button startStopButton;
+    boolean started = false;
+
+    RecordAudio recordTask;
+
+    ImageView imageView;
+    Bitmap bitmap;
+    Canvas canvas;
+    Paint paint;
+
+    private double sum=0;
+    private int cnt=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+
+        startStopButton = (Button) findViewById(R.id.record_button);
+        startStopButton.setOnClickListener(this);
+
+        transformer = new ca.uol.aig.fftpack.RealDoubleFFT(blockSize);
+
+        // ImageView 및 관련 객체 설정 부분
+        // 이미지뷰를 참조하고, 비트맵을 생성한다 비트맵의 첫번째 두번째 인수가 가로,세로의 크기이며
+        // ARGB_8888가 나타내는 의미는 각 픽셀은 4바이트에 저장된다이다
+        imageView = (ImageView) findViewById(R.id.record_image);
+        bitmap = Bitmap.createBitmap((int)320, (int)200,
+                Bitmap.Config.ARGB_8888);
+        // 비트맵 이미지를 불러와 캔버스에 그림을 그림
+        canvas = new Canvas(bitmap);
+        paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        // 비트맵 객체를 이용하여 이미지뷰를 보여줌
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+    private class RecordAudio extends AsyncTask<Void, double[], Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // AudioRecord를 설정하고 사용한다.
+                int bufferSize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
+
+                AudioRecord audioRecord = new AudioRecord( MediaRecorder.AudioSource.MIC, frequency,
+                        channelConfiguration, audioEncoding, bufferSize);
+
+                // short로 이뤄진 배열인 buffer는 원시 PCM 샘플을 AudioRecord 객체에서 받는다.
+                // double로 이뤄진 배열인 toTransform은 같은 데이터를 담지만 double 타입인데, FFT
+                // 클래스에서는 double타입이 필요해서이다.
+                short[] buffer = new short[blockSize]; //blockSize = 256
+                double[] toTransform = new double[blockSize]; //blockSize = 256
+
+                audioRecord.startRecording();
+
+                while (started) {
+                    int bufferReadResult = audioRecord.read(buffer, 0, blockSize); //blockSize = 256
+                    Log.i("bufferReadResult", Integer.toString(bufferReadResult));
+                    // AudioRecord 객체에서 데이터를 읽은 다음에는 short 타입의 변수들을 double 타입으로
+                    // 바꾸는 루프를 처리한다.
+                    // 직접 타입 변환(casting)으로 이 작업을 처리할 수 없다. 값들이 전체 범위가 아니라 -1.0에서
+                    // 1.0 사이라서 그렇다
+                    // short를 32,767(Short.MAX_VALUE) 으로 나누면 double로 타입이 바뀌는데,
+                    // 이 값이 short의 최대값이기 때문이다.
+                    for (int i = 0; i < blockSize && i < bufferReadResult; i++) {
+                        toTransform[i] = (double) buffer[i] / Short.MAX_VALUE; // 부호 있는 16비트
+                        Log.i("buffer", Double.toString(buffer[i]));
+                        Log.i("Short.MAX_VALUE", Short.toString(Short.MAX_VALUE));
+                        Log.i("toTransform", Double.toString(toTransform[i]));
+                    }
+
+                    // 이제 double값들의 배열을 FFT 객체로 넘겨준다. FFT 객체는 이 배열을 재사용하여 출력 값을 담는다
+                    // 포함된 데이터는 시간 도메인이 아니라
+                    // 주파수 도메인에 존재한다. 이 말은 배열의 첫 번째 요소가 시간상으로 첫 번째 샘플이 아니라는 얘기다.
+                    // 배열의 첫 번째 요소는 첫 번째 주파수 집합의 레벨을 나타낸다.
+
+                    // 256가지 값(범위)을 사용하고 있고 샘플 비율이 8,000 이므로 배열의 각 요소가 대략
+                    // 15.625Hz를 담당하게 된다. 15.625라는 숫자는 샘플 비율을 반으로 나누고(캡쳐할 수 있는
+                    // 최대 주파수는 샘플 비율의 반이다. <- 누가 그랬는데...), 다시 256으로 나누어 나온 것이다.
+                    // 따라서 배열의 첫 번째 요소로 나타난 데이터는 영(0)과 15.625Hz 사이에
+                    // 해당하는 오디오 레벨을 의미한다.
+
+                    transformer.ft(toTransform);
+                    // publishProgress를 호출하면 onProgressUpdate가 호출된다.
+                    publishProgress(toTransform);
+                }
+
+                audioRecord.stop();
+            } catch (Throwable t) {
+                Log.e("AudioRecord", "Recording Failed");
+            }
+
+            return null;
+        }
+        @Override
+        protected void onProgressUpdate(double[]... toTransform) {
+            canvas.drawColor(Color.BLACK);
+
+            double local_sum=0;
+            double deci_sum=0;
+
+            double local_max=0;
+            int max_i=-1;
+
+            for (int i = 0; i < toTransform[0].length; i++) {
+                int x = i;
+                int downy = (int) (100 - (toTransform[0][i] * 10));
+                int upy = 100;
+                int test1 = (int) (toTransform[0][i] * 100);
+
+                if(test1 > 2000){
+                    Log.e("주파수 : ",test1+"HZ");
+                }
+
+                if(test1>mcompare)
+                    mcompare=test1;
+                if(local_max<Math.abs(toTransform[0][i]))
+                {
+                    local_max=Math.abs(toTransform[0][i]);
+                    max_i=i;
+                }
+
+                /*local_sum+=(Math.abs(toTransform[0][i]) * i*15.625);
+                deci_sum+=(Math.abs(toTransform[0][i]));*/
+                canvas.drawLine(x, downy, x, upy, paint);
+            }
+            Log.i("local : ",local_sum+"HZ");
+            Log.i("deci : ",deci_sum+"HZ");
+//            local_sum=local_sum/toTransform[0].length;
+            /*if (deci_sum == 0){
+                local_sum = 0;
+            }else {
+                local_sum = local_sum / deci_sum;
+            }
+            sum+=local_sum;*/
+
+            sum+=((double)max_i*15.625);
+
+            cnt++;
+            imageView.invalidate();
+        }
+    }
+    @Override
+    public void onClick(View arg0) {
+        if (started) {
+            started = false;
+            startStopButton.setText("Start");
+            recordTask.cancel(true);
+            String text=Double.toString(sum/cnt);
+            sum=0;
+            cnt=0;
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        } else {
+            started = true;
+            startStopButton.setText("Stop");
+            recordTask = new RecordAudio();
+            recordTask.execute();
+        }
+    }
+}
--- myapplication/src/main/java/com/example/myapplication/fragments/PageThreeFragment.java
+++ myapplication/src/main/java/com/example/myapplication/fragments/PageThreeFragment.java
@@ -27,6 +27,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,8 +39,11 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.GpsTracker;
+import com.example.myapplication.ListAdapter;
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.Registration;
+import com.example.myapplication.Room;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -49,36 +53,35 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 import static android.content.Context.LOCATION_SERVICE;
 
-public class PageThreeFragment extends Fragment implements View.OnClickListener {
-    int mcompare=0;
-    int frequency = 8000;
-    int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
-    int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+public class PageThreeFragment extends Fragment {
 
-    private ca.uol.aig.fftpack.RealDoubleFFT transformer;
-    int blockSize = 256;
+    ListView mListView;
+    String connectUrl = "http://192.168.0.100:3001";
 
-    Button startStopButton;
-    boolean started = false;
+    ArrayList<String> room_list=new ArrayList<>();
 
-    RecordAudio recordTask;
-
-    ImageView imageView;
-    Bitmap bitmap;
-    Canvas canvas;
-    Paint paint;
-
-    private double sum=0;
-    private int cnt=0;
-
+    Socket mSocket;
+    String username="";
     public PageThreeFragment() {
     }
     @Override
@@ -86,138 +89,94 @@ public class PageThreeFragment extends Fragment implements View.OnClickListener 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.page_three_fragment,container,false);
-        startStopButton = (Button) rootview.findViewById(R.id.record_button);
-        startStopButton.setOnClickListener(this);
+        ListAdapter mMyAdapter = new ListAdapter(getActivity());
+        mListView = (ListView)rootview.findViewById(R.id.room_list);
+        mListView.setAdapter(mMyAdapter);
 
-        // RealDoubleFFT 클래스 컨스트럭터는 한번에 처리할 샘플들의 수를 받는다.
-        // 그리고 출력될 주파수 범위들의 수를 나타낸다. 위에서 설정한 256개의 샘플을 RealDobleFFT클래스로 보내준다
-        transformer = new ca.uol.aig.fftpack.RealDoubleFFT(blockSize);
+        new getRoom().execute(connectUrl + "/getRoom");
+        for(int i=0;i<room_list.size();i++)
+            mMyAdapter.addItem(ContextCompat.getDrawable(getContext(), R.drawable.vfdvdfdfv),room_list.get(i), room_list.get(i));
 
-        // ImageView 및 관련 객체 설정 부분
-        // 이미지뷰를 참조하고, 비트맵을 생성한다 비트맵의 첫번째 두번째 인수가 가로,세로의 크기이며
-        // ARGB_8888가 나타내는 의미는 각 픽셀은 4바이트에 저장된다이다
-        imageView = (ImageView) rootview.findViewById(R.id.record_image);
-        bitmap = Bitmap.createBitmap((int)320, (int)200,
-                Bitmap.Config.ARGB_8888);
-        // 비트맵 이미지를 불러와 캔버스에 그림을 그림
-        canvas = new Canvas(bitmap);
-        paint = new Paint();
-        paint.setColor(Color.YELLOW);
-        // 비트맵 객체를 이용하여 이미지뷰를 보여줌
-        imageView.setImageBitmap(bitmap);
+        Button enterButton = (Button) rootview.findViewById(R.id.enter_button);
+
+        enterButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Room.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
         return rootview;
     }
 
-    private class RecordAudio extends AsyncTask<Void, double[], Void> {
+    public class getRoom extends AsyncTask<String, String, String>{
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected String doInBackground(String... urls) {
             try {
-                // AudioRecord를 설정하고 사용한다.
-                int bufferSize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
 
-                AudioRecord audioRecord = new AudioRecord( MediaRecorder.AudioSource.MIC, frequency,
-                        channelConfiguration, audioEncoding, bufferSize);
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
 
-                // short로 이뤄진 배열인 buffer는 원시 PCM 샘플을 AudioRecord 객체에서 받는다.
-                // double로 이뤄진 배열인 toTransform은 같은 데이터를 담지만 double 타입인데, FFT
-                // 클래스에서는 double타입이 필요해서이다.
-                short[] buffer = new short[blockSize]; //blockSize = 256
-                double[] toTransform = new double[blockSize]; //blockSize = 256
+                try{
+                    //URL url = new URL("http://192.168.25.16:3000/users");
+                    URL url = new URL(urls[0]);
+                    con = (HttpURLConnection) url.openConnection();
+                    con.connect();
 
-                audioRecord.startRecording();
+                    InputStream stream = con.getInputStream();
 
-                while (started) {
-                    int bufferReadResult = audioRecord.read(buffer, 0, blockSize); //blockSize = 256
-                    Log.i("bufferReadResult", Integer.toString(bufferReadResult));
-                    // AudioRecord 객체에서 데이터를 읽은 다음에는 short 타입의 변수들을 double 타입으로
-                    // 바꾸는 루프를 처리한다.
-                    // 직접 타입 변환(casting)으로 이 작업을 처리할 수 없다. 값들이 전체 범위가 아니라 -1.0에서
-                    // 1.0 사이라서 그렇다
-                    // short를 32,767(Short.MAX_VALUE) 으로 나누면 double로 타입이 바뀌는데,
-                    // 이 값이 short의 최대값이기 때문이다.
-                    for (int i = 0; i < blockSize && i < bufferReadResult; i++) {
-                        toTransform[i] = (double) buffer[i] / Short.MAX_VALUE; // 부호 있는 16비트
-                        Log.i("buffer", Double.toString(buffer[i]));
-                        Log.i("Short.MAX_VALUE", Short.toString(Short.MAX_VALUE));
-                        Log.i("toTransform", Double.toString(toTransform[i]));
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
                     }
 
-                    // 이제 double값들의 배열을 FFT 객체로 넘겨준다. FFT 객체는 이 배열을 재사용하여 출력 값을 담는다
-                    // 포함된 데이터는 시간 도메인이 아니라
-                    // 주파수 도메인에 존재한다. 이 말은 배열의 첫 번째 요소가 시간상으로 첫 번째 샘플이 아니라는 얘기다.
-                    // 배열의 첫 번째 요소는 첫 번째 주파수 집합의 레벨을 나타낸다.
+                    return buffer.toString();
 
-                    // 256가지 값(범위)을 사용하고 있고 샘플 비율이 8,000 이므로 배열의 각 요소가 대략
-                    // 15.625Hz를 담당하게 된다. 15.625라는 숫자는 샘플 비율을 반으로 나누고(캡쳐할 수 있는
-                    // 최대 주파수는 샘플 비율의 반이다. <- 누가 그랬는데...), 다시 256으로 나누어 나온 것이다.
-                    // 따라서 배열의 첫 번째 요소로 나타난 데이터는 영(0)과 15.625Hz 사이에
-                    // 해당하는 오디오 레벨을 의미한다.
-
-                    transformer.ft(toTransform);
-                    // publishProgress를 호출하면 onProgressUpdate가 호출된다.
-                    publishProgress(toTransform);
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-
-                audioRecord.stop();
-            } catch (Throwable t) {
-                Log.e("AudioRecord", "Recording Failed");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
             return null;
         }
 
-        // onProgressUpdate는 우리 엑티비티의 메인 스레드로 실행된다. 따라서 아무런 문제를 일으키지 않고 사용자
-        // 인터페이스와 상호작용할 수 있다.
-        // 이번 구현에서는 onProgressUpdate가 FFT 객체를 통해 실행된 다음 데이터를 넘겨준다. 이 메소드는 최대
-        // 100픽셀의 높이로 일련의 세로선으로
-        // 화면에 데이터를 그린다. 각 세로선은 배열의 요소 하나씩을 나타내므로 범위는 15.625Hz다. 첫 번째 행은 범위가 0에서
-        // 15.625Hz인 주파수를 나타내고,
-        // 마지막 행은 3,984.375에서 4,000Hz인 주파수를 나타낸다.
-
         @Override
-        protected void onProgressUpdate(double[]... toTransform) {
-            canvas.drawColor(Color.BLACK);
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if(result.equals("empty"))
+                return ;
+            JsonParser jsonParser=new JsonParser();
+            JsonArray jsonArray = (JsonArray) jsonParser.parse(result);
 
-            double local_sum=0;
-
-            for (int i = 0; i < toTransform[0].length; i++) {
-                int x = i;
-                int downy = (int) (100 - (toTransform[0][i] * 10));
-                int upy = 100;
-                int test1 = (int) (toTransform[0][i] * 100);
-
-                if(test1 > 2000){
-                    Log.e("주파수 : ",test1+"HZ");
-                }
-
-                if(test1>mcompare)
-                    mcompare=test1;
-                local_sum+=(Math.abs(toTransform[0][i]) * 100);
-                canvas.drawLine(x, downy, x, upy, paint);
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JsonObject object = (JsonObject) jsonArray.get(i);
+                room_list.add(object.get("name").getAsString());
             }
-            local_sum=local_sum/toTransform[0].length;
-            sum+=local_sum;
-            cnt++;
-            imageView.invalidate();
         }
     }
 
-    @Override
-    public void onClick(View arg0) {
-        if (started) {
-            started = false;
-            startStopButton.setText("Start");
-            recordTask.cancel(true);
-            String text=Double.toString(sum/cnt);
-            sum=0;
-            cnt=0;
-            Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
-        } else {
-            started = true;
-            startStopButton.setText("Stop");
-            recordTask = new RecordAudio();
-            recordTask.execute();
-        }
-    }
 }
